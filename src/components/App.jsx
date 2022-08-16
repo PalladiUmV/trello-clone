6ca8a6c1735cc90266182c2b6de3ae42d68f58e3
ImNotCryingOverYou <limited886@gmail.com>
2022-08-16 20:13:32 +0300

    initial and add some styles

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import TrelloList from "./TrelloList";
+
+function App() {
+  return (
+    <div className="App">
+      <TrelloList title='Hello' />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TrelloCard.jsx
@@ -0,0 +1,12 @@
+import { Card, Typography } from '@mui/material'
+import React from 'react'
+
+const TrelloCard = () => {
+	return (
+		<Card>
+			<Typography gutterBottom> Word of the Day </Typography>
+		</Card>
+	)
+}
+
+export default TrelloCard
